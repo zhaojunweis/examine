894db393120936989cde7507f42f2f0deaa04974
wencaixu <JedisXu@163.com>
2019-03-26 12:56:18 +0800

    修改Bug，SubmitMapper缺少@Component注解

--- src/main/java/com/examine/dao/SubmitMapper.java
+++ src/main/java/com/examine/dao/SubmitMapper.java
@@ -1,7 +1,10 @@
 package com.examine.dao;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Map;
 
+@Component
 public interface SubmitMapper {
    void insertStudentLoginMessage(Map<String,String> map);
 }
