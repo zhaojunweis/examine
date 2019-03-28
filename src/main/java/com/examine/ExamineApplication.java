9238c555d25cef3620bb580dfcc62aec9473a70e
wencaixu <JedisXu@163.com>
2019-03-28 13:25:16 +0800

    修改起始页

--- src/main/java/com/examine/ExamineApplication.java
+++ src/main/java/com/examine/ExamineApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExamineApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ExamineApplication.class, args);
     }
 }
--- src/main/java/com/examine/config/ServletInitializer.java
+++ src/main/java/com/examine/config/ServletInitializer.java
@@ -5,7 +5,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
